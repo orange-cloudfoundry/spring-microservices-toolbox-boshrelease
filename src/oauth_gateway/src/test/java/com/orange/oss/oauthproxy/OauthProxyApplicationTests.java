0e466fa0d30374f2d69842cd85598bb3129aa1b6
Pierre Oblin <pierre.oblin@orange.com>
2016-05-12 12:05:23 +0200

    add maven source for oauth_gateway

---
+++ src/oauth_gateway/src/test/java/com/orange/oss/oauthproxy/OauthProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orange.oss.oauthproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OauthProxyApplication.class)
+public class OauthProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
